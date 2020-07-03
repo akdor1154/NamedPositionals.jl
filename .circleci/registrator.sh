83ea55d37cbd374d7934ade9600b2d1d5fed7899
Jarrad Whitaker <akdor1154@gmail.com>
2020-07-03 19:28:42 +1000

    temp: register on registrator branch

---
+++ .circleci/registrator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+repoPath="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
+hash="${CIRCLE_SHA1}"
+token="${GITHUB_TOKEN}"
+
+curl -fL \
+    -u :"${token}" \
+    -d '{"body": "[CircleCI-triggered]\nJuliaRegistrator register"}' \
+    -X POST \
+    "https://api.github.com/repos/${repoPath}/commits/${hash}/comments"
